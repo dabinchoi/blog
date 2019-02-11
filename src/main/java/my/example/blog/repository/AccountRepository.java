882effc18b3c009971ce46cf05d004744d00cbb3
test <dabinnn1011@naver.com>
2019-02-11 18:46:00 +0900

    회원가입 commit

--- src/main/java/my/example/blog/repository/AccountRepository.java
+++ src/main/java/my/example/blog/repository/AccountRepository.java
@@ -1,12 +1,14 @@
 package my.example.blog.repository;
 
 
+
 import my.example.blog.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    @Query("SELECT a from Acoount a WHERE a.email = :email")
+
+    @Query("SELECT a from Account a WHERE a.email = :email")
     public Account getAccountByEmail(@Param("email") String email);
 }
--- src/main/java/my/example/blog/security/BlogUserDetailsService.java
+++ src/main/java/my/example/blog/security/BlogUserDetailsService.java
@@ -17,30 +17,28 @@ import java.util.List;
 import java.util.Set;
 
 @Component
-@RequiredArgsConstructor //final 인 필드만 초기화하는 생성자가 자동으로 만들어진다. 생성자주입시 사용
+@RequiredArgsConstructor // final 인 필드만 초기화하는 생성자가 자동으로 만들어진다. 생성자주입시 사용
 public class BlogUserDetailsService implements UserDetailsService {
     private final AccountRepository accountRepository;
 
     @Override
     @Transactional(readOnly = false)
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Account account = accountRepository.getAccountByEmail(email);
-        if (account == null)
-            throw new UsernameNotFoundException(email + "에 해당하는 사람이 없다");
+        Account account =
+                accountRepository.getAccountByEmail(email);
+        if(account == null)
+            throw new UsernameNotFoundException(email + "에 해당하는 사용자 없어!");
 
         List<GrantedAuthority> authorities = new ArrayList<>();
         Set<Role> roles = account.getRoles();
-        for (Role role : roles) {
+        for(Role role : roles){
             authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
-
         }
-
+        // 리턴한 값은 세션에 저장된다.
         BlogSecurityUser blogSecurityUser = new BlogSecurityUser(email, account.getPasswd(), authorities);
         blogSecurityUser.setId(account.getId());
         blogSecurityUser.setName(account.getName());
         blogSecurityUser.setNickName(account.getNickName());
-
         return blogSecurityUser;
-
     }
 }
